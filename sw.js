ea864ea45321d2e37766c77a11fd54916ca49f5f
MrStevenbot <mr.bryanstevenn@gmail.com>
2026-04-21 03:44:19 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-const CACHE = 'agrocalc-v2';
-const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'agrocalc-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
@@ -16,7 +13,14 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
+  // Siempre intenta red primero, caché solo si no hay conexión
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    fetch(e.request)
+      .then(res => {
+        var copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
